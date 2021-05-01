99a4f506bfe9ead1f1838c1a677a5fb008029280
BuildTools <unconfigured@null.spigotmc.org>
2021-05-01 16:12:37 -0500

    chatformat updates

--- index.js
+++ index.js
@@ -33,8 +33,9 @@ bot.ftop = {
     ftop:[],
     ptop:[]
 }
-//bot.chatAddPattern(/^\[\[(?:[^ ]*)] ([^ ]*) -> me\] (.*)$/, "dm", "archon dm") actually for heavenmc
+bot.chatAddPattern(/^\[\[(?:[^ ]*)] ([^ ]*) -> me\] (.*)$/, "dm", "archon dm") 
 bot.chatAddPattern(/^\[(?:[^ ]*)] \(([^ ]*) ➥ me\) (.*)$/, "dm", "archon dm")
+
 bot.chatAddPattern(/^(?:[*+]{1,3}|[^ ]*)(?: |)(?:[ ]{0,1})([^ ]*): (.*)$/, "fcf", "archon fcf")
 bot.chatAddPattern(/\[!] WE ARE GETTING RAIDED \[!]/, "raid", "archon raid alerts")
 const vec3 = require('vec3')
