a5f981e5a73522b5169fc26741cbf5dafdc63752
BuildTools <unconfigured@null.spigotmc.org>
2021-05-07 21:13:37 -0500

    asd

--- index.js
+++ index.js
@@ -3386,8 +3386,8 @@ client.on('message', async (message) => {
         .setTitle(`:musical_note: Song queue for ${message.guild.name}`)
         .setColor(guild.embedColor)
         .setTimestamp()
-        .setDescription(`**Currently Playing:** ${songs[0]}\n\n**Up Next:**\n${songs.length > 1 ? songs.slice(1).splice(songs[0],songs[5]).join("\n") : "No songs after the current one"}`)
-        .setThumbnail(song_info.thumbnail_url)
+        .setDescription(`**Currently Playing:** ${songs[0]}\n\n**Up Next:**\n${songs.length > 1 ? songs.slice(1).splice(songs[0],songs[songs.length <= 5 ? songs.length : 5]).join("\n") : "No songs after the current one"}`)
+        .setThumbnail(song_info.thumbnail_url)<=
         message.channel.send(embed)
     }
     if(commandName == "nowplaying" || commandName == "np"){
