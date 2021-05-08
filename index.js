bd39b90404a85934783ba8266fe05a282432219a
BuildTools <unconfigured@null.spigotmc.org>
2021-05-07 20:52:12 -0500

    ok

--- index.js
+++ index.js
@@ -334,8 +334,8 @@ const video_player = async (g, guild, song) => {
             video_player(g, guild, song_queue.songs[0]);
         }
         else{
-            server_queue.songs = [];
-            server_queue.connection.dispatcher.end()
+            song_queue.songs = [];
+            song_queue.connection.dispatcher.end()
             let embed = new Discord.MessageEmbed()
             .setColor(g.embedColor)
             .setTimestamp()
