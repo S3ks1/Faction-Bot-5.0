3b781d316562bbe346bec162d99a7778cfccaef9
xling <klingenfelt@gmail.com>
2021-05-29 00:41:47 -0500

    fix maybe

--- index.js
+++ index.js
@@ -942,67 +942,70 @@ bot.on('login', async () => {
         let buffers = await getUserByBufferCheck()
         let rpost = await getUserByRpostCheck()
         let guild = await getGuild(config.mainGuild)
-
-                if(parseInt(time-walls[0].lastwallcheck) % parseInt(guild.wallAlert) == 0 && guild.walls === true && guild.grace == false){
-                    if(ms((time-walls[0].lastwallcheck)*1000, { long: true }) == "0 ms") return;
-                    let g = client.guilds.cache.get(config.mainGuild)
-                    let c = g.channels.cache.find(c => c.name === guild.wallChannel)
-                    bot.chat(`Walls have not been checked in ${ms((time-walls[0].lastwallcheck)*1000, { long: true })}! Check now and type ${guild.prefix}${guild.wallCommand}`)
-                    if(c){
-                        let embed = new Discord.MessageEmbed()
-                        .setColor(guild.embedColor)
-                        .setTimestamp()
-                        .setDescription(`:warning: Walls have been unchecked for ${ms((time-walls[0].lastwallcheck)*1000, { long: true })}! Check now by typing ${guild.prefix}${guild.wallCommand}`)
-                        c.send(embed)
-                        if(guild.alertRole !== "none"){
-                            let r = g.roles.cache.find(r=>r.name===guild.alertRole)
-                            if(r){
-                                c.send(`<@&${r.id}>`)
-                                c.send(`<@&${r.id}>`)
+                if(walls !== undefined){
+                    if(parseInt(time-walls[0].lastwallcheck) % parseInt(guild.wallAlert) == 0 && guild.walls === true && guild.grace == false){
+                        if(ms((time-walls[0].lastwallcheck)*1000, { long: true }) == "0 ms") return;
+                        let g = client.guilds.cache.get(config.mainGuild)
+                        let c = g.channels.cache.find(c => c.name === guild.wallChannel)
+                        bot.chat(`Walls have not been checked in ${ms((time-walls[0].lastwallcheck)*1000, { long: true })}! Check now and type ${guild.prefix}${guild.wallCommand}`)
+                        if(c){
+                            let embed = new Discord.MessageEmbed()
+                            .setColor(guild.embedColor)
+                            .setTimestamp()
+                            .setDescription(`:warning: Walls have been unchecked for ${ms((time-walls[0].lastwallcheck)*1000, { long: true })}! Check now by typing ${guild.prefix}${guild.wallCommand}`)
+                            c.send(embed)
+                            if(guild.alertRole !== "none"){
+                                let r = g.roles.cache.find(r=>r.name===guild.alertRole)
+                                if(r){
+                                    c.send(`<@&${r.id}>`)
+                                    c.send(`<@&${r.id}>`)
+                                }
                             }
                         }
                     }
                 }
-                if(parseInt(time-buffers[0].lastbuffercheck) % parseInt(guild.bufferAlert) == 0 && guild.buffers === true && guild.grace == false){
-                    if(ms((time-buffers[0].lastbuffercheck)*1000, { long: true }) == "0 ms") return;
-                    let g = client.guilds.cache.get(config.mainGuild)
-                    let c = g.channels.cache.find(c => c.name === guild.bufferChannel)  
-                    bot.chat(`Buffers have not been checked in ${ms((time-buffers[0].lastbuffercheck)*1000, { long: true })}! Check now and type ${guild.prefix}${guild.bufferCommand}`)
-                    if(c){
-                        let embed = new Discord.MessageEmbed()
-                        .setColor(guild.embedColor)
-                        .setTimestamp()
-                        .setDescription(`:warning: Buffers have been unchecked for ${ms((time-buffers[0].lastbuffercheck)*1000, { long: true })}! Check now by typing ${guild.prefix}${guild.bufferCommand}`)
-                        c.send(embed)
-                        if(guild.alertRole !== "none"){
-                            let r = g.roles.cache.find(r=>r.name===guild.alertRole)
-                            if(r){
-                                message.channel.send(r)
+                if(buffers !== undefined){
+                    if(parseInt(time-buffers[0].lastbuffercheck) % parseInt(guild.bufferAlert) == 0 && guild.buffers === true && guild.grace == false){
+                        if(ms((time-buffers[0].lastbuffercheck)*1000, { long: true }) == "0 ms") return;
+                        let g = client.guilds.cache.get(config.mainGuild)
+                        let c = g.channels.cache.find(c => c.name === guild.bufferChannel)  
+                        bot.chat(`Buffers have not been checked in ${ms((time-buffers[0].lastbuffercheck)*1000, { long: true })}! Check now and type ${guild.prefix}${guild.bufferCommand}`)
+                        if(c){
+                            let embed = new Discord.MessageEmbed()
+                            .setColor(guild.embedColor)
+                            .setTimestamp()
+                            .setDescription(`:warning: Buffers have been unchecked for ${ms((time-buffers[0].lastbuffercheck)*1000, { long: true })}! Check now by typing ${guild.prefix}${guild.bufferCommand}`)
+                            c.send(embed)
+                            if(guild.alertRole !== "none"){
+                                let r = g.roles.cache.find(r=>r.name===guild.alertRole)
+                                if(r){
+                                    message.channel.send(r)
+                                }
                             }
                         }
                     }
                 }
-                if(parseInt(time-rpost[0].lastrpostcheck) % parseInt(guild.rpostAlert) == 0 && guild.rpost === true && guild.grace == false){
-                    if(ms((time-rpost[0].lastrpostcheck)*1000, { long: true }) == "0 ms") return;
-                    let g = client.guilds.cache.get(config.mainGuild)
-                    let c = g.channels.cache.find(c => c.name === guild.rpostChannel)
-                    bot.chat(`RPost walls have not been checked in ${ms((time-rpost[0].lastrpostcheck)*1000, { long: true })}! Check now and type ${guild.prefix}${guild.rpostCommand}`)
-                    if(c){
-                        let embed = new Discord.MessageEmbed()
-                        .setColor(guild.embedColor)
-                        .setTimestamp()
-                        .setDescription(`:warning: RPost walls have been unchecked for ${ms((time-rpost[0].lastrpostcheck)*1000, { long: true })}! Check now by typing ${guild.prefix}${guild.rpostCommand}`)
-                        c.send(embed)
-                        if(guild.alertRole !== "none"){
-                            let r = g.roles.cache.find(r=>r.name===guild.alertRole)
-                            if(r){
-                                message.channel.send(r)
+                if(rpost !== undefined){
+                    if(parseInt(time-rpost[0].lastrpostcheck) % parseInt(guild.rpostAlert) == 0 && guild.rpost === true && guild.grace == false){
+                        if(ms((time-rpost[0].lastrpostcheck)*1000, { long: true }) == "0 ms") return;
+                        let g = client.guilds.cache.get(config.mainGuild)
+                        let c = g.channels.cache.find(c => c.name === guild.rpostChannel)
+                        bot.chat(`RPost walls have not been checked in ${ms((time-rpost[0].lastrpostcheck)*1000, { long: true })}! Check now and type ${guild.prefix}${guild.rpostCommand}`)
+                        if(c){
+                            let embed = new Discord.MessageEmbed()
+                            .setColor(guild.embedColor)
+                            .setTimestamp()
+                            .setDescription(`:warning: RPost walls have been unchecked for ${ms((time-rpost[0].lastrpostcheck)*1000, { long: true })}! Check now by typing ${guild.prefix}${guild.rpostCommand}`)
+                            c.send(embed)
+                            if(guild.alertRole !== "none"){
+                                let r = g.roles.cache.find(r=>r.name===guild.alertRole)
+                                if(r){
+                                    message.channel.send(r)
+                                }
                             }
                         }
                     }
                 }
-            
-        
     }, 1000)
 
     setInterval(async () => {
